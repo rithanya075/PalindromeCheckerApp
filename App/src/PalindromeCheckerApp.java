3ad430de5f22ecb8370bf3933b995be19be95662
rithanyaaa075 <rs2190@srmist.edu.in>
2026-03-16 21:22:59 +0530

    UC11: Object-Oriented Palindrome Service using Encapsulation

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,28 @@
 import java.util.Scanner;
+import java.util.Stack;
+
+// Service class
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for(int i = 0; i < input.length(); i++){
+            stack.push(input.charAt(i));
+        }
+
+        // Compare with original
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) != stack.pop()){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
 
 public class PalindromeCheckerApp {
 
@@ -9,26 +33,12 @@ public class PalindromeCheckerApp {
         System.out.println("Enter a string:");
         String input = sc.nextLine();
 
-        // Normalize string: remove spaces & convert to lowercase
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        // Create object of service class
+        PalindromeChecker checker = new PalindromeChecker();
 
-        int start = 0;
-        int end = normalized.length() - 1;
+        boolean result = checker.checkPalindrome(input);
 
-        boolean isPalindrome = true;
-
-        while(start < end) {
-
-            if(normalized.charAt(start) != normalized.charAt(end)) {
-                isPalindrome = false;
-                break;
-            }
-
-            start++;
-            end--;
-        }
-
-        if(isPalindrome) {
+        if(result){
             System.out.println("The string is a Palindrome.");
         } else {
             System.out.println("The string is NOT a Palindrome.");
