ee86fbfcc994d552b8ef6d0e07113070474b4977
rithanyaaa075 <rs2190@srmist.edu.in>
2026-03-16 21:33:02 +0530

    UC13: Performance comparison of palindrome algorithms

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,99 +1,72 @@
-import java.util.*;
-
-// Strategy Interface
-interface PalindromeStrategy {
-    boolean checkPalindrome(String input);
-}
-
-// Stack Strategy
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean checkPalindrome(String input) {
-
-        Stack<Character> stack = new Stack<>();
-
-        for(char c : input.toCharArray()){
-            stack.push(c);
-        }
-
-        for(char c : input.toCharArray()){
-            if(c != stack.pop()){
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-// Deque Strategy
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean checkPalindrome(String input) {
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for(char c : input.toCharArray()){
-            deque.addLast(c);
-        }
-
-        while(deque.size() > 1){
-            if(deque.removeFirst() != deque.removeLast()){
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-// Context Class
-class PalindromeChecker {
-
-    private PalindromeStrategy strategy;
-
-    public PalindromeChecker(PalindromeStrategy strategy){
-        this.strategy = strategy;
-    }
-
-    public boolean check(String input){
-        return strategy.checkPalindrome(input);
-    }
-}
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
+    // Method 1: Reverse String
+    public static boolean reverseMethod(String str) {
+        String reversed = "";
+
+        for(int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        return str.equals(reversed);
+    }
+
+    // Method 2: Stack Method
+    public static boolean stackMethod(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : str.toCharArray()) {
+            stack.push(c);
+        }
+
+        for(char c : str.toCharArray()) {
+            if(c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Method 3: Two Pointer Method
+    public static boolean twoPointerMethod(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while(start < end) {
+            if(str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        String input = "madam";
 
-        System.out.println("Enter a string:");
-        String input = sc.nextLine();
+        // Reverse Method Timing
+        long startTime = System.nanoTime();
+        boolean r1 = reverseMethod(input);
+        long endTime = System.nanoTime();
+        System.out.println("Reverse Method Time: " + (endTime - startTime) + " ns");
 
-        System.out.println("Choose Strategy:");
-        System.out.println("1. Stack Strategy");
-        System.out.println("2. Deque Strategy");
+        // Stack Method Timing
+        startTime = System.nanoTime();
+        boolean r2 = stackMethod(input);
+        endTime = System.nanoTime();
+        System.out.println("Stack Method Time: " + (endTime - startTime) + " ns");
 
-        int choice = sc.nextInt();
-
-        PalindromeStrategy strategy;
-
-        if(choice == 1){
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
-
-        PalindromeChecker checker = new PalindromeChecker(strategy);
-
-        boolean result = checker.check(input);
-
-        if(result){
-            System.out.println("The string is a Palindrome.");
-        } else {
-            System.out.println("The string is NOT a Palindrome.");
-        }
-
-        sc.close();
+        // Two Pointer Timing
+        startTime = System.nanoTime();
+        boolean r3 = twoPointerMethod(input);
+        endTime = System.nanoTime();
+        System.out.println("Two Pointer Method Time: " + (endTime - startTime) + " ns");
     }
 }
